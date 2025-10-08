f32ed90c1d4a270f596466c2139e549b13585114
Oranji1 <oranjidev@gmail.com>
2025-10-08 20:49:45 -0300

    Improve SitesURLs type checking

--- core/models/anime.py
+++ core/models/anime.py
@@ -1,17 +1,32 @@
 from datetime import date, time
-from typing import Literal, Optional
+from typing import Annotated, Literal, Optional
 
+from pydantic import AfterValidator, BaseModel, HttpUrl
 from pydantic.types import PositiveInt
 
 from core.models.base_model import BaseModel
 from core.models.enums import AiringStatus, MediaType, SourceType
 
 
+def check_host(url: HttpUrl, allowed_host: str) -> HttpUrl:
+    if url.host != allowed_host:
+        raise TypeError(
+            f"Invalid host URL. Expected '{allowed_host}', got '{url.host}'."
+        )
+    return url
+
+
+def host_url_validator(host: str):
+    return AfterValidator(lambda url: check_host(url, host))
+
+
 class SitesURLs(BaseModel):
-    anidb: Optional[str] = None
-    anilist: Optional[str] = None
-    ann: Optional[str] = None
-    mal: Optional[str] = None
+    anidb: Optional[Annotated[HttpUrl, host_url_validator("anidb.net")]] = None
+    anilist: Optional[Annotated[HttpUrl, host_url_validator("anilist.co")]] = None
+    ann: Optional[
+        Annotated[HttpUrl, host_url_validator("www.animenewsnetwork.com")]
+    ] = None
+    mal: Optional[Annotated[HttpUrl, host_url_validator("myanimelist.net")]] = None
 
 
 class AnimeAiringInfo(BaseModel):
