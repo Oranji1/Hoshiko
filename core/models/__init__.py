b262b5a52330cd328c751f40e6440a394b0e1584
Oranji1 <oranjidev@gmail.com>
2025-10-16 22:21:36 -0300

    Add enums import to core/models/__init__.py

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,3 +1,4 @@
 from .anime import Anime, AnimeAiringInfo, SitesURLs
+from .enums import AiringStatus, MediaType, SourceType
 
-__all__ = ["Anime", "AnimeAiringInfo", "SitesURLs"]
+__all__ = ["AiringStatus", "Anime", "AnimeAiringInfo", "MediaType", "SitesURLs", "SourceType"]
