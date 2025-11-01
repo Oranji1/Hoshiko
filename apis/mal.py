4251eaae3ce13377d9df015852ff2c2781c99161
Oranji1 <oranjidev@gmail.com>
2025-10-31 21:11:46 -0300

    Remove JikanPy as a dependency and use pure Niquests for Jikan API requests

--- apis/mal.py
+++ apis/mal.py
@@ -1,11 +1,17 @@
-from jikanpy import AioJikan
+import niquests
 
 
-async def get_anime(mal_id: int) -> dict:
-    async with AioJikan() as jikan:
-        return await jikan.anime(mal_id, extension="full")
+async def get_anime(id: int) -> dict:
+    async with niquests.AsyncSession() as session:
+        url = f"https://api.jikan.moe/v4/anime/{id}/full"
+        response = await session.get(url=url)
+
+        return response.json()
 
 
-async def get_manga(mal_id: int) -> dict:
-    async with AioJikan() as jikan:
-        return await jikan.manga(mal_id, extension="full")
+async def get_manga(id: int) -> dict:
+    async with niquests.AsyncSession() as session:
+        url = f"https://api.jikan.moe/v4/manga/{id}/full"
+        response = await session.get(url=url)
+
+        return response.json()
